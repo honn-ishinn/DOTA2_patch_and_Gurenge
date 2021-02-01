3751ff082756e84c7b2048e0f51a22b97de2c29a
honn-ishinn <shihonglance@gmail.com>
2021-02-02 02:18:14 +0800

    Codes for gathering data and their outputs

--- scripts/a_Gurenge_image.R
+++ scripts/a_Gurenge_image.R
@@ -14,8 +14,41 @@
 
 library(tesseract)
 library(tidyverse)
+library(here)
 
 # check if Japanese language is available, if not need to install it
-tesseract_info()
+# based on https://cran.r-project.org/web/packages/tesseract/vignettes/intro.html
 
-#### 
+"jpn" %in% tesseract_info()$available
+
+# tesseract_download("jpn")
+
+
+#### Import the image and change it to text ####
+
+
+# Huuuuuge thanks to Raffael in stackoverflow, solved my issue of saving the output txt file
+# Based on the link:https://stackoverflow.com/questions/19877676/write-utf-8-files-from-r/20097594#20097594?newreg=302b07937be145109caf409e45654af3
+
+
+gurenge_text <- tesseract::ocr(here::here("inputs/image/gurenge_kashi.jpg"), engine = tesseract("jpn"))
+
+Encoding(gurenge_text) <- "UTF-16"
+
+write.table(gurenge_text,"outputs/gurenge_text/gurenge_kashi.txt",row.names=F,col.names=F,quote=F)
+
+# a wrong example
+
+wrong_text <- tesseract::ocr(here::here("inputs/image/gurenge_kashi.jpg"), engine = tesseract("jpn"))
+
+cat(wrong_text, file = "outputs/gurenge_text/wrong_text.txt")
+
+
+
+
+
+
+
+
+
+
